ad5c826294cb7b8888ee2afd794f62376e3f8a1c
Abirami Moorthy <53604043+Abirami8799@users.noreply.github.com>
2021-06-08 14:21:11 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -61,3 +61,21 @@ class TestFactorial(unittest.TestCase):
         self.assertEqual(res,120)
         if __name__ == '__main__':
         unittest.main()
+        
+        
+        
+        
+  import sys
+  def fact(n):
+    if n==0:
+      return 1
+    retunrn n*fact(n-1)
+   def div(n):
+    res=10/n
+    return res
+  def main(n):
+    res=fact(n)
+    print(res)
+  if __name__=='__main__':
+    if len(sys.argv)>1:
+      main(int(sys.argv[1]))
