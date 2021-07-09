02bafba35250279c773fbd69b1d1d09933ceac5b
Abirami Moorthy <53604043+Abirami8799@users.noreply.github.com>
2021-07-09 14:52:22 +0530

    Add files via upload

---
+++ lettermatchfile.py
@@ -0,0 +1,45 @@
+
+
+import os
+key = input("enter the word  ")
+letter=input("enter the first letter of word")
+dict = {'3': 'len_3' , '4': 'len_4', '5':'len_5'}  
+if key in dict:
+   b=dict[key]
+a=b
+s=[]
+for root, dirs, files in os.walk("/home/abirami/Music/findfolder", topdown=False):
+    for name in dirs:
+        if name == a:
+           a=os.path.join(root,name)
+           for root, dirs, files in os.walk(a):
+               for val in files:
+                   if val.startswith(letter):
+                      s.append(os.path.join(root,val))
+        
+
+file=open(s[0],"r")
+word=file.read()
+print(word)
+ 
+
+
+
+
+
+                      
+        
+
+
+           
+               
+           
+
+
+
+
+
+
+        
+
+
